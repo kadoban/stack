cb12a50812bbc4c55ef0d277260d5adf1b08fcfc
Joshua Simmons <joshua.simmons@emptypath.com>
2015-12-11 20:54:23 -0700

    test unicode nonsense flags

--- test/integration/tests/1337-unicode-everywhere/Main.hs
+++ test/integration/tests/1337-unicode-everywhere/Main.hs
@@ -3,3 +3,5 @@ import StackTest
 main :: IO ()
 main = do stack ["build"]
           stack ["exec", "以-exe"]
+          stackErr ["build", "--flag", "以:लकरतअ"]
+          stack    ["build", "--flag", "以:लकरतअ", "--flag", "以:恵比毛勢須"]
--- test/integration/tests/1337-unicode-everywhere/files/ארץ/Пσε.hs
+++ test/integration/tests/1337-unicode-everywhere/files/ארץ/Пσε.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE CPP #-}
 module Пσε
     ( θυπε
     ) where
 
 θυπε :: String
+#ifdef BREAK_THINGS
+θυπε = "以呂波耳本部止" ++ otherThing
+#else
 θυπε = "以呂波耳本部止"
+#endif
+
+#ifdef FIX_THINGS
+otherThing :: String
+otherThing = "stuff"
+#endif
