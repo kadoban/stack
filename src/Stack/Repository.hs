183dcffb701f6b0637fc5290a10a0580c865aa2c
Joshua Simmons <joshua.simmons@emptypath.com>
2015-12-22 18:07:40 -0700

    create Repository class
    Will be used to represent remote repositories that can be locally
    cached, for example using git, svn, bzr, hg, and also somewhat more
    limited ones such as a remote HTTP "directory", and also a local
    directory.
    
    Basically it should encompass anything where you can retreive files
    from, possibly at a selected version, and cache them for repeated
    consumption.

---
+++ src/Stack/Repository.hs
@@ -0,0 +1,56 @@
+-- |
+-- Module      :  Stack.Repository
+-- Copyright   :  Joshua Simmons <joshua.simmons@emptypath.com> 2015
+-- License     :  BSD3
+--
+-- An abstraction of a repository of files that may be versioned and can be
+-- usefully locally cached.
+--
+module Stack.Repository where
+
+import           Control.Monad.Catch (MonadCatch, MonadThrow)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           Data.Text (Text)
+import qualified Data.Text.IO as T
+import           Path
+
+class Repository r where
+    cacheRepo :: (MonadIO m, MonadThrow m, MonadCatch m)
+              => CacheLoc -> r -> m ()
+    cacheRepoFile :: (MonadIO m, MonadThrow m, MonadCatch m)
+                  => CacheLoc -> r -> Path Rel File -> m Text
+    cacheRepoFile cache repo file = do cacheRepo cache repo
+                                       dir <- cacheTo cache repo
+                                       liftIO $ T.readFile
+                                                     (toFilePath $ dir </> file)
+    repoType :: r -> String
+    fullUri :: r -> String
+    parseRepo :: (MonadThrow m)
+              => String -> Maybe String -> m r
+
+cacheTo' :: (MonadThrow m, Repository r) => r -> m (Path Rel Dir)
+cacheTo' = undefined
+
+cacheTo :: (MonadThrow m, Repository r) => CacheLoc -> r -> m (Path Abs Dir)
+cacheTo (DirectlyIn a) _ = return a
+cacheTo (OrganizeIn a) r = (a </>) <$> cacheTo' r
+
+data CacheLoc = DirectlyIn (Path Abs Dir) | OrganizeIn (Path Abs Dir)
+    deriving (Eq, Ord, Show)
+
+data GitRepo = GitRepo String         -- ^ URL of the remote repository
+                       (Maybe String) -- ^ Version to use, if not the default
+    deriving (Eq, Ord, Show)
+
+data HttpRepo = HttpRepo String
+    deriving (Eq, Ord, Show)
+
+instance Repository HttpRepo where
+    repoType _ = "rawhttp"
+    fullUri (HttpRepo s) = s
+    parseRepo url _ = return $ HttpRepo url
+
+instance Repository GitRepo where
+    repoType _ = "git"
+    fullUri (GitRepo s _) = s
+    parseRepo url ver = return $ GitRepo url ver
